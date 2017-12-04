0db111b378901be9f0064ae910ef82377d466171
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-12-04 07:32:43 -0800

    Imported the Vision framework and created a variable that will hold an array of the vision requests.

--- day1-canvas/day1-canvas/ViewController.swift
+++ day1-canvas/day1-canvas/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var canvasView: CanvasView!
     @IBOutlet weak var digitLabel: UILabel!
     
+    var requests = [VNRequest]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
