bad2153b322b6bc7c7a2ecd89428abe91482adec
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-12-04 07:00:19 -0800

    Removed placeholder text.

--- day1-canvas/day1-canvas/CanvasView.swift
+++ day1-canvas/day1-canvas/CanvasView.swift
@@ -56,13 +56,4 @@ class CanvasView: UIView {
         self.layer.sublayers = nil
         self.setNeedsDisplay()
     }
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
