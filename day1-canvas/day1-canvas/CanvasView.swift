ba8e98c8c19c1423ce358965281d94a0e62c64e9
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-12-04 06:33:02 -0800

    CanvasView 1.) Created CanvasView class. 2.) Added in necessary variables we will need. 3.) Linked UIView to CanvasView class in our inspector tool.

---
+++ day1-canvas/day1-canvas/CanvasView.swift
@@ -0,0 +1,31 @@
+//
+//  CanvasView.swift
+//  day1-canvas
+//
+//  Created by Adrian Kenepah-Martin on 12/4/17.
+//  Copyright © 2017 Adrian Kenepah-Martin. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    
+    var lineColor:UIColor!
+    var lineWidth:CGFloat!
+    var path:UIBezierPath!
+    var touchPoint:CGPoint!
+    var startingPoint:CGPoint!
+    
+    override func layoutSubviews() {
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
