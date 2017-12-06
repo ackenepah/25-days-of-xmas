d6a4385c38a727017a260c31af8d5db9df5940db
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-12-06 11:42:44 -0800

    Recognize Number using Vision Framework 1.) Setup an array for our requests. 2.) Created a function called setupVision. 2.) Setup completion handler that will get our array of VNRequests which we then convert every item within that array into a VNClassificationObservation by using flatMap. Using .filter we are taking each object from the array and accessing it's confidence property to get the results of 80%. When then .map through the classifications array and only grab the identifiers (digits) that we want to display to the user. 3.) When the recognizeDigit button is clicked we will grab the image that was drawn by the user and store that into an image variable by running our canvasView function. Then we use the scaleImage function to scale down the image to 28 x 28 pixels.

--- day1-canvas/day1-canvas/ViewController.swift
+++ day1-canvas/day1-canvas/ViewController.swift
@@ -18,13 +18,51 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupVision()
     }
+    
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: MNIST().model) else { fatalError("Cannot load Vision ML Model.") }
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: self.handleClassification)
+        self.requests = [classificationRequest]
+    }
+    
+    func handleClassification(request: VNRequest, error: Error?) {
+        guard let observations = request.results else { fatalError("There are no results."); return }
+        
+        let classification = observations
+            .flatMap({ $0 as? VNClassificationObservation })
+            .filter({ $0.confidence > 0.8 })
+            .map({ $0.identifier })
+        
+        DispatchQueue.main.async {
+            self.digitLabel.text = classification.first
+        }
+    }
+    
     @IBAction func clearCanvas(_ sender: UIButton) {
         canvasView.clearCanvas()
     }
     
     @IBAction func recognizeDigit(_ sender: UIButton) {
+        let image = UIImage(view: canvasView)
+        let scaledImage = scaleImage(image: image, toSize: CGSize(width: 28, height: 28))
+        
+        let imageRequestHandler = VNImageRequestHandler(cgImage: scaledImage.cgImage!, options: [:])
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func scaleImage (image: UIImage, toSize size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
     }
 }
 
