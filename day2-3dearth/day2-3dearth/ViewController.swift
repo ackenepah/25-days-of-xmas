d400b20f1f7ef50a926781a7e41dbcf8ec4c6b54
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-12-14 00:24:18 -0800

    EarthNode Not Displaying Reason the EarthNode object wasn't displaying was because the Scene in our storyboard was an AR view.
    1.) Removed the previous code I wrote.
    2.) Removed the placeholder scene object (the spaceship)
    3.) Created our new scene constant.

--- day2-3dearth/day2-3dearth/ViewController.swift
+++ day2-3dearth/day2-3dearth/ViewController.swift
@@ -16,43 +16,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let scene = SCNScene()
-        
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
-        
-        scene.rootNode.addChildNode(cameraNode)
-        
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light?.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 2)
-        
-        scene.rootNode.addChildNode(lightNode)
-        
-        let stars = SCNParticleSystem(named: "StarsParticles.scnp", inDirectory: nil)!
-        scene.rootNode.addParticleSystem(stars)
-        
-        let earthNode = EarthNode()
-        scene.rootNode.addChildNode(earthNode)
-        
-        let sceneView = self.view as! SCNView
-        sceneView.scene = scene
-        
-        sceneView.showsStatistics = true
-        sceneView.backgroundColor = UIColor.black
-        sceneView.allowsCameraControl = true
         
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
